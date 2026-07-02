e05e78466a8ceffc1945ebe4024048f5671d6a88
fardousmoha <arawellohosny6@gamil.com>
2026-07-02 09:59:30 +0300

    Add Exercise 1 and Exercise 2

---
+++ Exercise 1/main.js
@@ -0,0 +1,19 @@
+let x = 7;
+let y = 3;
+
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+console.log(x % y);
+
+//increament
+
+console.log(x++);
+console.log(x);
+
+
+//decreament
+
+console.log(y--);
+console.log(y)
---
+++ Exercise 2/main.js
@@ -0,0 +1,18 @@
+let a =15;
+let b ="15";
+
+
+console.log(a == b);  // Output: true (loose equality)
+console.log(a != b);  // Output: false
+
+
+
+console.log(a === b); // Output: false (strict equality)
+console.log(a !== b); // Output: true
+
+
+
+console.log(a > b);   // Output: false
+console.log(a < b);   // Output: false
+console.log(a >= b);  // Output: true
+console.log(a <= b);  // Output: true
