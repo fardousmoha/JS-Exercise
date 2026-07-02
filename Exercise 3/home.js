94169a0326181cfab79c66c740f50e0f20da77c6
fardousmoha <arawellohosny6@gamil.com>
2026-07-02 10:42:20 +0300

    add exercise 3

---
+++ Exercise 3/home.js
@@ -0,0 +1,13 @@
+let isSunny = true;
+let isWeekend = false;
+
+// AND Operators
+console.log(isSunny && isWeekend); 
+
+
+// OR Operators
+console.log(isSunny || isWeekend);
+
+//inverse
+console.log(!isSunny);
+console.log(!isWeekend);
