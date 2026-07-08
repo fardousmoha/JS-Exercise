37eae072efab16b916a771a96428671496abcaf1
fardousmoha <arawellohosny6@gamil.com>
2026-07-08 10:28:19 +0300

    add exercise 5

---
+++ Exercise 5/main.js
@@ -0,0 +1,8 @@
+const addNumbers = (a, b) => {
+  return a + b;
+}
+
+console.log(addNumbers(2, 3));
+console.log(addNumbers(10, 5));
+console.log(addNumbers(1.5, 1));
+console.log(addNumbers(3, 4));
