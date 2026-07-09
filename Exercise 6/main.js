e23d6feb5e7e65d3c55277755e7de8c213ed227a
fardousmoha <arawellohosny6@gamil.com>
2026-07-09 11:01:30 +0300

    add exercise 6

---
+++ Exercise 6/main.js
@@ -0,0 +1,12 @@
+
+
+// arrays and objects
+
+let colors = ["red", "green", "blue","brown","black","white"];
+
+console.log(colors[0]); 
+console.log(colors[1]);
+console.log(colors[2]);
+console.log(colors[3]);
+console.log(colors[4]);
+console.log(colors[5]);
