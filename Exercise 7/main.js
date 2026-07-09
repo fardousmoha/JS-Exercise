ec527b56bd2a874fbae8f620a6f3e4729aa22c14
fardousmoha <arawellohosny6@gamil.com>
2026-07-09 11:28:03 +0300

    adding exercise 7

---
+++ Exercise 7/main.js
@@ -0,0 +1,45 @@
+
+
+// arrays and objects
+
+let colors = ["red", "green", "blue","brown","black","white"];
+
+console.log(colors[0]); 
+console.log(colors[1]);
+console.log(colors[2]);
+console.log(colors[3]);
+console.log(colors[4]);
+console.log(colors[5]);
+
+//push
+colors.push("yellow");
+console.log(colors);
+
+// length after push
+console.log(colors.length);
+
+
+
+
+//pop
+colors.pop();
+console.log(colors);
+
+
+//unshift
+colors.unshift("purple");
+console.log(colors);
+
+// length after unshift
+console.log(colors.length);
+
+
+
+
+//shift
+colors.shift();
+console.log(colors);
+
+
+//length
+console.log(colors.length);
