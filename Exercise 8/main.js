3ae53e7c267ed67d64b0ca3018c8ced64beee434
fardousmoha <arawellohosny6@gamil.com>
2026-07-11 09:50:08 +0300

    adding exercise 8

---
+++ Exercise 8/main.js
@@ -0,0 +1,13 @@
+let car ={
+    year: 2020,
+    make: "Toyota",
+    model: "Camry",
+    engine: "2.5L I4",
+    transmission: "8-speed automatic",
+    
+}
+console.log(car.year);
+console.log(car.make);
+console.log(car.model);
+console.log(car.engine);
+console.log(car.transmission);
