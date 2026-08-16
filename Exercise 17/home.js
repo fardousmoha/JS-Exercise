44387dfc0f27531cca8c9a1fdd99baea11a76c42
fardousmoha <arawellohosny6@gamil.com>
2026-08-16 21:20:53 +0300

    adding exercise 17

---
+++ Exercise 17/home.js
@@ -0,0 +1,10 @@
+let temp = 25;
+if (temp < 0) {
+    console.log("very cold");
+} else if (temp < 15) {
+    console.log("Cold");
+} else if (temp < 25) {
+    console.log("Normal");
+} else {
+    console.log("Hot");
+}
