0c5f1492a57ff879cce3325ecf49f893ad65f153
fardousmoha <arawellohosny6@gamil.com>
2026-09-06 22:43:41 +0300

    adding exercise 24

---
+++ Exercise 24/home.js
@@ -0,0 +1,10 @@
+function calculateArea(length, width) {
+    return length * width;
+}
+const length = 5;
+const width = 10;
+const area = calculateArea(length, width);
+console.log(`The area is: ${area}`);
+
+
+  
